f3485d5998892caf017dcc3854d0178b60c211e1
Camilo Segura <camilo.segura@prodigious.com>
2017-05-26 10:42:05 -0500

    Create Markup code and test cases

--- src/scripts/modules/nav/markup.js
+++ src/scripts/modules/nav/markup.js
@@ -1,10 +1,11 @@
 class Markup {
   constructor() {
+    this.$ul = document.createElement('ul');
     this.$li = document.createElement('li');
     this.$a = document.createElement('a');
   }
 
-  createItem(item = { label: '', url: '' }) {
+  createItem(item = { label: '', url: '', items: [] }) {
     const $li = this.$li.cloneNode();
     const $a = this.$a.cloneNode();
 
@@ -13,7 +14,67 @@ class Markup {
 
     $li.appendChild($a);
 
+    if (item.items && item.items.length > 0) {
+      $li.appendChild(this.createItems(item.items));
+    }
+
     return $li;
+  }
+
+  createItems(items = []) {
+    const $ul = this.$ul.cloneNode();
+
+    items.forEach((item) => {
+      $ul.appendChild(this.createItem(item));
+    });
+    return $ul;
+  }
+
+  addClass(html, tag, className) {
+    const $elements = html.getElementsByTagName(tag);
+    const length = $elements.length;
+    let i;
+
+    if (html.tagName.toLowerCase() === tag.toLowerCase()) {
+      html.setAttribute('class', className);
+    }
+
+    for (i = 0; i < length; i++) {
+      $elements[i].setAttribute('class', className);
+    }
+
+    return html;
+  }
+
+  addClassByLevel(html, tag, className, level) {
+    const children = html.childNodes;
+
+    if (level === 1) {
+      if (tag === 'ul') {
+        html.setAttribute('class', className);
+      } else {
+        children.forEach((child) => {
+          if (tag === 'a') {
+            child.children[0].setAttribute('class', className);
+          } else if (tag === 'li') {
+            child.setAttribute('class', className);
+          }
+        });
+      }
+    } else {
+      children.forEach((child) => {
+        const innerChildren = child.childNodes;
+        const innerLevel = level - 1;
+
+        innerChildren.forEach((innerChild) => {
+          if (innerChild.tagName.toLowerCase() === 'ul') {
+            this.addClassByLevel(innerChild, tag, className, innerLevel);
+          }
+        });
+      });
+    }
+
+    return html;
   }
 }
 
--- test/markup.test.js
+++ test/markup.test.js
@@ -1,13 +1,95 @@
 import Markup from '../src/scripts/modules/nav/markup';
 
-const markup = new Markup();
+let markup = new Markup();
+let html;
 
-describe('Create item', () => {
-  const item = {
+const item = {
+  label: 'What we do',
+  url: '#/about/what-we-do',
+};
+
+const items1 = [
+  {
     label: 'What we do',
     url: '#/about/what-we-do',
-  };
+  },
+];
 
+const items11 = [
+  {
+    label: 'What we do',
+    url: '#/about/what-we-do',
+    items: [
+      {
+        label: 'How we work',
+        url: '#/about/how-we-work',
+      },
+    ],
+  },
+];
+
+const items3 = [
+  {
+    label: 'What we do',
+    url: '#/about/what-we-do',
+  },
+  {
+    label: 'How we work',
+    url: '#/about/how-we-work',
+  },
+  {
+    label: 'Leadership',
+    url: '#/about/leadership',
+  },
+];
+
+const items2 = [
+  {
+    label: 'What we do',
+    url: '#/about/what-we-do',
+    items: [
+      {
+        label: 'How we work',
+        url: '#/about/how-we-work',
+      },
+      {
+        label: 'Leadership',
+        url: '#/about/leadership',
+      },
+    ],
+  },
+];
+
+const items22 = [
+  {
+    label: 'What we do',
+    url: '#/about/what-we-do',
+    items: [
+      {
+        label: 'How we work',
+        url: '#/about/how-we-work',
+      },
+      {
+        label: 'Leadership',
+        url: '#/about/leadership',
+      },
+    ],
+  },
+  {
+    label: 'Work',
+    url: '#/work',
+    items: [
+
+    ],
+  },
+];
+
+beforeEach(() => {
+  markup = new Markup();
+  html = markup.createItems(items2);
+});
+
+describe('Create item', () => {
   it('should be an item', () => {
     expect(typeof markup.createItem(item)).toBe('object');
   });
@@ -27,4 +109,117 @@ describe('Create item', () => {
   it('should a filled item', () => {
     expect(markup.createItem(item).outerHTML).toBe('<li><a href="#/about/what-we-do">What we do</a></li>');
   });
+});
+
+describe('Create items', () => {
+  it('Should be an empty list', () => {
+    expect(markup.createItems().outerHTML).toBe('<ul></ul>');
+  });
+
+  it('Should be a list with 1 items', () => {
+    expect(markup.createItems(items1).outerHTML).toBe('<ul><li><a href="#/about/what-we-do">What we do</a></li></ul>');
+  });
+
+  it('Should be a list with 3 items', () => {
+    expect(markup.createItems(items3).getElementsByTagName('li').length).toBe(3);
+  });
+});
+
+describe('Create items with sub items', () => {
+  it('Should be a list with 1 sub item', () => {
+    expect(markup.createItems(items11).outerHTML).toBe('<ul><li><a href="#/about/what-we-do">What we do</a><ul><li><a href="#/about/how-we-work">How we work</a></li></ul></li></ul>');
+  });
+
+  it('Should be a list with 2 sub items', () => {
+    expect(markup.createItems(items2).outerHTML).toBe('<ul><li><a href="#/about/what-we-do">What we do</a><ul><li><a href="#/about/how-we-work">How we work</a></li><li><a href="#/about/leadership">Leadership</a></li></ul></li></ul>');
+  });
+});
+
+describe('Modify element', () => {
+  describe('Add classes', () => {
+    let tag;
+    let className;
+
+    it('To anchors', () => {
+      className = 'link';
+      tag = 'a';
+
+      expect(markup.addClass(html, tag, className).outerHTML).toBe('<ul><li><a href="#/about/what-we-do" class="link">What we do</a><ul><li><a href="#/about/how-we-work" class="link">How we work</a></li><li><a href="#/about/leadership" class="link">Leadership</a></li></ul></li></ul>')
+    });
+
+    it('To lists', () => {
+      className = 'lists';
+      tag = 'li';
+
+      expect(markup.addClass(html, tag, className).outerHTML).toBe('<ul><li class="lists"><a href="#/about/what-we-do">What we do</a><ul><li class="lists"><a href="#/about/how-we-work">How we work</a></li><li class="lists"><a href="#/about/leadership">Leadership</a></li></ul></li></ul>');
+    });
+
+    it('To UL', () => {
+      className = 'UL';
+      tag = 'ul';
+
+      expect(markup.addClass(html, tag, className).outerHTML).toBe('<ul class="UL"><li><a href="#/about/what-we-do">What we do</a><ul class="UL"><li><a href="#/about/how-we-work">How we work</a></li><li><a href="#/about/leadership">Leadership</a></li></ul></li></ul>')
+    });
+  });
+
+  describe('Add Classes to first level', () => {
+    let tag;
+    let className;
+    const level = 1;
+
+    beforeEach(() => {
+      markup = new Markup();
+      html = markup.createItems(items22);
+    });
+
+    it('To anchors', () => {
+      className = 'link';
+      tag = 'a';
+
+      expect(markup.addClassByLevel(html, tag, className, level).outerHTML).toBe('<ul><li><a href="#/about/what-we-do" class="link">What we do</a><ul><li><a href="#/about/how-we-work">How we work</a></li><li><a href="#/about/leadership">Leadership</a></li></ul></li><li><a href="#/work" class="link">Work</a></li></ul>');
+    });
+
+    it('To lists', () => {
+      className = 'lists';
+      tag = 'li';
+
+      expect(markup.addClassByLevel(html, tag, className, level).outerHTML).toBe('<ul><li class="lists"><a href="#/about/what-we-do">What we do</a><ul><li><a href="#/about/how-we-work">How we work</a></li><li><a href="#/about/leadership">Leadership</a></li></ul></li><li class="lists"><a href="#/work">Work</a></li></ul>');
+    });
+
+    it('To UL', () => {
+      className = 'UL';
+      tag = 'ul';
+
+      expect(markup.addClassByLevel(html, tag, className, level).outerHTML).toBe('<ul class="UL"><li><a href="#/about/what-we-do">What we do</a><ul><li><a href="#/about/how-we-work">How we work</a></li><li><a href="#/about/leadership">Leadership</a></li></ul></li><li><a href="#/work">Work</a></li></ul>');
+    });
+  });
+
+  describe('Add Classes to second level', () => {
+    let tag;
+    let className;
+    const level = 2;
+
+    it('To anchors', () => {
+      className = 'link';
+      tag = 'a';
+
+      expect(markup.addClassByLevel(html, tag, className, level).outerHTML).toBe('<ul><li><a href="#/about/what-we-do">What we do</a><ul><li><a href="#/about/how-we-work" class="link">How we work</a></li><li><a href="#/about/leadership" class="link">Leadership</a></li></ul></li></ul>')
+    });
+
+    it('To lists', () => {
+      className = 'lists';
+      tag = 'li';
+
+      expect(markup.addClassByLevel(html, tag, className, level).outerHTML).toBe('<ul><li><a href="#/about/what-we-do">What we do</a><ul><li class="lists"><a href="#/about/how-we-work">How we work</a></li><li class="lists"><a href="#/about/leadership">Leadership</a></li></ul></li></ul>');
+    });
+
+    it('To UL', () => {
+      className = 'UL';
+      tag = 'ul';
+
+      expect(markup.addClassByLevel(html, tag, className, level).outerHTML).toBe('<ul><li><a href="#/about/what-we-do">What we do</a><ul class="UL"><li><a href="#/about/how-we-work">How we work</a></li><li><a href="#/about/leadership">Leadership</a></li></ul></li></ul>')
+    });
+  });
+  // TODO
+  // describe('Add attributes', () => {})
 });
